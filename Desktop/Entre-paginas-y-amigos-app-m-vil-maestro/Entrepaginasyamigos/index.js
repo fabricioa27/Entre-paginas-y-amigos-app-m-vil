066b7cc0ff968a725cee97a3e9dd8b5d19e50c70
charlyluna17 <lunadiazroberto6@gmail.com>
2026-05-20 22:49:34 -0600

    se creo el js de LoginScren para el frontend del login

--- Desktop/Entre-paginas-y-amigos-app-m-vil-maestro/Entrepaginasyamigos/index.js
+++ Desktop/Entre-paginas-y-amigos-app-m-vil-maestro/Entrepaginasyamigos/index.js
@@ -1,5 +1,8 @@
-// UBICACIÓN: Entrepaginasyamigos/index.js
 import { registerRootComponent } from 'expo';
-import App from './App'; // Conecta con App.js en la raíz
 
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
 registerRootComponent(App);
